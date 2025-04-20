839de63455080ac96b43254d20c37b60f2309114
KwaiSeng-Cisco <KwaiSengLai@hotmail.com>
2025-04-20 17:01:24 +0800

    Added featurex : a python file to add 2 input variables

---
+++ add2number.py
@@ -0,0 +1,10 @@
+# This program adds two numbers
+
+num1 = input('Enter first lucky number : ')
+num2 = input('Enter second lucky number : ')
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
