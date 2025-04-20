6c988a34422209ed1eaf70c99099b1175d0d309c
kwaiseng <kslai@cisco.com>
2025-04-20 18:24:23 +0800

    Added Time Service

--- js/hostname.js
+++ js/hostname.js
@@ -1,10 +1,15 @@
 
 function printhost(){
     var host = location.host;
-    document.getElementById("host").innerHTML = host
+    document.getElementById("host").innerHTML = host;
 }
 
 function printhostname(){
     var hostname = location.hostname;
-    document.getElementById("hostname").innerHTML = hostname
+    document.getElementById("hostname").innerHTML = hostname;
+}
+
+function current_time(){
+    var date = Date();
+    document.getElementById("date").innerHTML = date;
 }
