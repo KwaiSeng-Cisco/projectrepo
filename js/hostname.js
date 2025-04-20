772088641e07c5dfd2c5ccd03b22581cb5783279
KwaiSeng-Cisco <KwaiSengLai@hotmail.com>
2025-04-20 17:14:51 +0800

    added showhostname feature

---
+++ js/hostname.js
@@ -0,0 +1,10 @@
+
+function printhost(){
+    var host = location.host;
+    document.getElementById("host").innerHTML = host
+}
+
+function printhostname(){
+    var hostname = location.hostname;
+    document.getElementById("hostname").innerHTML = hostname
+}
